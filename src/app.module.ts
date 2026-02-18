df50f6485e7e5dea3e8af1e1c0bc7b2d37e00206
Samad126 <samed192005@gmail.com>
2026-02-18 21:21:12 +0400

    feat: added exception filter for prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { DatabaseModule } from './common/database/database.module.js';
+import { APP_FILTER } from '@nestjs/core';
+import { PrismaClientExceptionFilter } from './common/filters/prisma-exception.filter.js';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: PrismaClientExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- src/common/database/database.service.ts
+++ src/common/database/database.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class DatabaseService extends PrismaClient {
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL as string,
     });
     super({ adapter });
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
   }
 }
