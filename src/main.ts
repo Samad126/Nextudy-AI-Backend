1d1372ed6096269b659de744129eea6485aa90a8
Samad126 <samed192005@gmail.com>
2026-03-17 23:59:33 +0400

    feat: implement cookie + bearer auth (access and refresh tokens)

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module.js';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+
+  app.use(cookieParser());
 
   app.enableCors({
     origin: process.env.ALLOWED_ORIGINS?.split(',') ?? false,
